0bd230f659bb5dbf8c257c921aa8a3b1fb8cf70d
freddyjs90 <fred.yys0123@gmail.com>
2021-10-22 11:34:20 -0500

    primer commit

---
+++ proyecto02/src/proyecto02/Proyecto02.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto02;
+
+/**
+ *
+ * @author SALAA
+ */
+public class Proyecto02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("ejemplo basico de java");
+        System.out.println("manejando desde github");
+        System.out.println("versionando a traves de git");
+        System.out.println("ademas de usar githubclassrom");
+    }
+
+}
